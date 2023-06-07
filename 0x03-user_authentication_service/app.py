a54a12ef8e34ab3bb7168d4f33bbd27c676f6e68
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-07 11:38:44 -0700

    Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Flask app that has a single GET route ("/") and use
+flask.jsonify to return a JSON payload of the form"""
+from flask import Flask, jsonify, request, abort, redirect
+form auth import Auth
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=Flash)
+def welcome():
+    """Basic Flask app, return a JSON"""
+    return jsonify({"message": "Bienvenue"})
