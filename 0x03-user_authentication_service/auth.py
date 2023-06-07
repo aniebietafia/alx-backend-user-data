65d1fa5ebc8d543a6445d81fc24b96b13f9fba12
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-07 11:27:13 -0700

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,3 +14,22 @@ def _hash_password(password: str) -> str:
     The returned string is a salted hash of the input password,
     hashed with bcrypt.hashpw"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Takes mandatory email and password string arguments and return a User object"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return user
+        else:
+            raise ValueError('User {email} already exists')
