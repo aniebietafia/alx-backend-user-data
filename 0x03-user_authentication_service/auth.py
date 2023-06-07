af0f74b46f1e66143fc27181ed222fd39f171464
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-07 12:03:13 -0700

    credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,13 @@ class Auth:
             return user
         else:
             raise ValueError('User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """credentials validation, return a boolean"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        else:
+            return bcrypt.checkpw(password=password.encode('utf-8'),
+                    hashed_password=user.hashed_password)
