10a2140da8fd8a035d109f150b2544e0845af1fc
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-08 06:05:28 -0700

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,5 +97,23 @@ def log_out() -> None:
     return redirect('/')
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """generate a token and respond with a 200 HTTP status"""
+    try:
+        email = request.form['email']
+    except KeyError:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "reset_token": reset_token}
+
+    return jsonify(msg), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
