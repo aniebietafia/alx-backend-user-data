ff604b0873ce763ef6d710fa4df432cce1859943
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-08 06:00:32 -0700

    log out functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,19 @@ def update_password():
     return jsonify({"email": email, "message": "Password updated"}), 200
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def log_out() -> None:
+    """Find the user with the requested session ID.
+    If the user exists destroy the session and redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
